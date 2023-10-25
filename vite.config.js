353b95672c23f8ebce0ccd74bcb495376d0ca139
unknown <yenb2012172@student.ctu.edu.vn>
2023-10-25 22:09:01 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,13 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    server: {
+      port: 3001,
+      proxy: {
+        "/api": {
+          target: "http://localhost:3000/",
+          changeOrigin: true,
+        },
+      }
     },
-  server: {
-    port: 3001,
-  },
-  }
 })
