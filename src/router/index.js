43b8ed84c2b8241dddc865660a86f2a4f62f03df
unknown <yenb2012172@student.ctu.edu.vn>
2023-11-01 22:28:58 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+import NotFound from "@/views/NotFound.vue";
 const routes = [
     {
         path: "/",
         name: "contactbook",
         component: ContactBook,
+    }, {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
     },
+        
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="page">
+        <p>
+            Opps, không thể tìm thấy trang. Trở về
+            <router-link to="/">trang chủ</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+export default{
+    name: NotFound,
+}
+</script>
